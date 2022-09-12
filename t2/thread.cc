fc8b5bfa5e84a61603010ebe26add5a4087e8ec3
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-09-12 15:35:07 -0300

    First implementation of Thread methods

---
+++ t2/thread.cc
@@ -0,0 +1,21 @@
+#include "thread.h"
+#include <iostream>
+#include <ucontext.h>
+
+__BEGIN_API
+
+int Thread::_next_id = 0;
+
+int Thread::switch_context(Thread *prev, Thread *next) {
+  _running = next;
+  CPU::switch_context(prev->context(), next->context());
+};
+
+void Thread::thread_exit(int exit_code) {
+  switch_context(this, _mainThread);
+  delete this;
+};
+
+int Thread::id() { return _id; }
+
+__END_API
