4980fbcc1de25b2291bc3e59db2434b8c5030713
Thiago Bewiahn <thiagoaugustobewiahn@gmail.com>
2022-09-13 08:42:24 -0300

    Corrected code from t1

--- t2/cpu.cc
+++ t2/cpu.cc
@@ -8,10 +8,13 @@ void CPU::Context::save() { getcontext(&_context); }
 
 void CPU::Context::load() { setcontext(&_context); }
 
-CPU::Context::~Context() { delete _stack; }
+CPU::Context::~Context() {
+  if (_stack)
+    delete _stack;
+}
 
 void CPU::switch_context(Context *from, Context *to) {
-    swapcontext(&from->_context, &to->_context);
+  swapcontext(&from->_context, &to->_context);
 }
 
 __END_API
--- t2/cpu.h
+++ t2/cpu.h
@@ -17,25 +17,9 @@ public:
   public:
     ucontext_t _context;
 
-    Context() {}
+    Context() { _stack = 0; };
 
-    template <typename... Tn> Context(void (*func)(Tn...), Tn... args) {
-      this->_stack = new char[this->STACK_SIZE];
-
-      if (!this->_stack) {
-        std::cout << "Erro ao alocar pilha\n";
-        exit(1);
-      }
-
-      // https://nitish712.blogspot.com/2012/10/thread-library-using-context-switching.html
-      getcontext(&this->_context);
-      this->_context.uc_link = 0;
-      this->_context.uc_stack.ss_sp = (void *)_stack;
-      this->_context.uc_stack.ss_size = this->STACK_SIZE;
-      this->_context.uc_stack.ss_flags = 0;
-
-      makecontext(&this->_context, (void (*)())(func), sizeof...(Tn), args...);
-    };
+    template <typename... Tn> Context(void (*func)(Tn...), Tn... args);
 
     ~Context();
 
@@ -45,6 +29,26 @@ public:
   };
 
   static void switch_context(Context *from, Context *to);
+};
+
+template <typename... Tn>
+inline CPU::Context::Context(void (*func)(Tn...), Tn... args) {
+  this->_stack = new char[this->STACK_SIZE];
+
+  if (!this->_stack) {
+    std::cout << "Error in creating stack\n";
+    exit(-1);
+  }
+
+  save();
+
+  // https://nitish712.blogspot.com/2012/10/thread-library-using-context-switching.html
+  this->_context.uc_link = 0;
+  this->_context.uc_stack.ss_sp = (void *)_stack;
+  this->_context.uc_stack.ss_size = this->STACK_SIZE;
+  this->_context.uc_stack.ss_flags = 0;
+
+  makecontext(&this->_context, (void (*)())(func), sizeof...(Tn), args...);
 };
 
 __END_API
